b22732c65f36c1eed68944f490d1cec13a1809a1
naoki-funawatari <waribasika@live.jp>
2020-04-05 20:41:55 +0900

    feat: ResourcesController 追加。

---
+++ Controllers/ResourcesController.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AwsDemo.Controllers
+{
+    public class ResourcesController : ApiController
+    {
+        // GET: api/Resources
+        public JObject Get()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+            using (var con = new SqlConnection(connectionString))
+            using (var cmd = new SqlCommand())
+            {
+                con.Open();
+                cmd.Connection = con;
+                cmd.Transaction = con.BeginTransaction(IsolationLevel.ReadCommitted);
+
+                var sql = "SELECT name FROM users WHERE id = @id AND password = @password;";
+                cmd.CommandText = sql;
+                cmd.Parameters.Clear();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new JObject {
+                            { "name", new JValue(reader["name"].ToString()) }
+                        };
+                    }
+                }
+            }
+
+            throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.Unauthorized));
+        }
+
+        // GET: api/Resources/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Resources
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Resources/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Resources/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
