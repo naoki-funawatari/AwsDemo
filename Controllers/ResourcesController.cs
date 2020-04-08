d40b23f98b87936fa2618fdafe39baee992d8ea2
naoki-funawatari <waribasika@live.jp>
2020-04-08 20:34:51 +0900

    feat: リソース取得 API 実装。

--- Controllers/ResourcesController.cs
+++ Controllers/ResourcesController.cs
@@ -1,10 +1,8 @@
 ﻿using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -14,74 +12,77 @@ namespace AwsDemo.Controllers
     public class ResourcesController : ApiController
     {
         // GET: api/Resources
-        public JObject Get([FromBody]JObject json)
+        public IEnumerable<JObject> Get()
         {
-            var newDate = DateTime.Parse(json["newDate"].ToString());
-            var view = json["view"].ToString();
-            var action = json["action"].ToString();
-
-
-            var connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
-            using (var con = new SqlConnection(connectionString))
-            using (var cmd = new SqlCommand())
+            var resouces = new List<JObject>();
+            try
             {
-                con.Open();
-                cmd.Connection = con;
-                cmd.Transaction = con.BeginTransaction(IsolationLevel.ReadCommitted);
+                //var newDate = DateTime.Parse(json["newDate"].ToString());
+                //var view = json["view"].ToString();
+                //var action = json["action"].ToString();
 
-                var sql = "SELECT name FROM users WHERE id = @id AND password = @password;";
-                cmd.CommandText = sql;
-                cmd.Parameters.Clear();
-                if (view == "month")
+                var connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+                using (var con = new SqlConnection(connectionString))
+                using (var cmd = new SqlCommand())
                 {
+                    con.Open();
+                    cmd.Connection = con;
+                    cmd.Transaction = con.BeginTransaction(IsolationLevel.ReadCommitted);
 
-                }
-                if (view == "week")
-                {
-
-                }
-                if (view == "day")
-                {
-
-                }
-                if (view == "agenda")
-                {
-
-                }
-
-                using (var reader = cmd.ExecuteReader())
-                {
-                    if (reader.Read())
+                    var sql = "SELECT id, title FROM resources;";
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Clear();
+                    using (var reader = cmd.ExecuteReader())
                     {
-                        return new JObject {
-                            { "name", new JValue(reader["name"].ToString()) }
-                        };
+                        while (reader.Read())
+                        {
+                            resouces.Add(new JObject {
+                                { "id", new JValue(reader.GetInt32(0)) },
+                                { "title", new JValue(reader.GetString(1)) },
+                            });
+                        }
                     }
+                    //if (view == "month")
+                    //{
+                    //}
+                    //if (view == "week")
+                    //{
+                    //}
+                    //if (view == "day")
+                    //{
+                    //}
+                    //if (view == "agenda")
+                    //{
+                    //}
                 }
+
+                return resouces;
             }
-
-            throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.Unauthorized));
+            catch
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.Unauthorized));
+            }
         }
 
-        // GET: api/Resources/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //// GET: api/Resources/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
-        // POST: api/Resources
-        public void Post([FromBody]string value)
-        {
-        }
+        //// POST: api/Resources
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
-        // PUT: api/Resources/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT: api/Resources/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE: api/Resources/5
-        public void Delete(int id)
-        {
-        }
+        //// DELETE: api/Resources/5
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
